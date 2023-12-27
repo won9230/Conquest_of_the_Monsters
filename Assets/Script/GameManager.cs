b461afc6b5a76a71b0e61fec9f3749c19da53711
won9230 <chldnjs8899@gmail.com>
2023-12-27 22:54:10 +0900

    no message

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,30 +5,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	public enum GameState
-	{
-		Field,
-		Battle
-	}
-	public GameState gameState = GameState.Field;
+	public static GameManager instance;
+
 	private void Awake()
 	{
-		//DontDestroyOnLoad(gameObject);		//씬이 바뀌어도 파괴되지 않음
-		//Cursor.visible = false;		//커서 안보이게 설정
-		//Cursor.lockState = CursorLockMode.Locked;	//커서 잠금
-	}
-
-	private void Update()
-	{
-		switch (gameState)
+		if(instance == null)
 		{
-			case (GameState.Field):
-
-				break;
-			case (GameState.Battle):
-
-				break;
+			instance = this;
 		}
+		else if(instance != this)
+		{
+			Destroy(gameObject);
+			Debug.Log("GameManager가 중복되서 삭제되었습니다");
+		}
+		DontDestroyOnLoad(gameObject);
 	}
-
 }

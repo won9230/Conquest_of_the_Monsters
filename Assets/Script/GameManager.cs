7046f7a71c3339115851edf34483b27c9c417f92
won9230 <chldnjs8899@gmail.com>
2024-01-11 23:03:57 +0900

    no message

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,6 +7,27 @@ public class GameManager : MonoBehaviour
 {
 	public static GameManager instance;
 
+	[System.Serializable]
+	public class RagionData
+	{
+		public string regionName;
+		public int maxAmountEnemys = 4;
+		public List<GameObject> possibleEnemys = new List<GameObject>();
+	}
+
+	public List<RagionData> ragions = new List<RagionData>();
+
+	private bool gotAttack = false;
+
+	public enum GameState
+	{
+		World_State,
+		Town_State,
+		Battle_State,
+		Idle
+	}
+	public GameState gamestate;
+
 	private void Awake()
 	{
 		if(instance == null)
@@ -19,5 +40,28 @@ public class GameManager : MonoBehaviour
 			Debug.Log("GameManager가 중복되서 삭제되었습니다");
 		}
 		DontDestroyOnLoad(gameObject);
+	}
+
+	private void Update()
+	{
+		switch (gamestate)
+		{
+			case GameState.World_State:
+				if (gotAttack)
+				{
+					gamestate = GameState.Battle_State;
+				}
+				break;
+			case GameState.Town_State: 
+				break;
+			case GameState.Battle_State:
+				//배틀 씬 로드
+
+				break;
+			case GameState.Idle: 
+				break;
+			default:
+			break;
+		}
 	}
 }
